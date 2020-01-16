6d207f16037dc90c15ea4f94270458dc2835c950
Francis Samande Declaro <>
2020-01-16 11:12:22 +0800

    Updates

---
+++ precache-manifest.9f6d04b0735a89a412237627d1bc778e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a02c008999e4c9adde76d03624d3fb70",
+    "url": "/tdd-cypress-hooks/index.html"
+  },
+  {
+    "revision": "5a530f0753dd8c4a0a85",
+    "url": "/tdd-cypress-hooks/static/css/main.714fd260.chunk.css"
+  },
+  {
+    "revision": "476240ea925b95766625",
+    "url": "/tdd-cypress-hooks/static/js/2.0efbac45.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tdd-cypress-hooks/static/js/2.0efbac45.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a530f0753dd8c4a0a85",
+    "url": "/tdd-cypress-hooks/static/js/main.ec90d60e.chunk.js"
+  },
+  {
+    "revision": "3ab11f17685d81acf0cb",
+    "url": "/tdd-cypress-hooks/static/js/runtime-main.829e6267.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/tdd-cypress-hooks/precache-manifest.9f6d04b0735a89a412237627d1bc778e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tdd-cypress-hooks/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
